317dfee1f1f3433a5543c409fec28cadfeb907cc
vojtech <p.prusavojta@gmail.com>
2022-05-16 14:40:55 +0200

    Change Postgres URL Version2

--- main.py
+++ main.py
@@ -112,7 +112,7 @@ class Comment(db.Model):
     post_id = db.Column(db.Integer, db.ForeignKey("blog_posts.id"))
     parent_post = relationship("BlogPost", back_populates="comments")
 
-# db.create_all()
+db.create_all()
 
 
 @app.route('/')
