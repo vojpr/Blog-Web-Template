1a57a89e0f7e5d6b342a2f905b55d0889aa38840
vojtech <p.prusavojta@gmail.com>
2022-05-16 14:02:09 +0200

    Change SQLite to Postgres

--- main.py
+++ main.py
@@ -24,17 +24,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-EMAIL = os.getenv('MY_EMAIL')
-EMAIL_PASSWORD = os.getenv('MY_EMAIL_PASSWORD')
+# For secrets stored in .env
+# EMAIL = os.getenv('MY_EMAIL')
+# EMAIL_PASSWORD = os.getenv('MY_EMAIL_PASSWORD')
+
+EMAIL = os.environ.get('MY_EMAIL')
+EMAIL_PASSWORD = os.environ.get('MY_EMAIL_PASSWORD')
 
 CURRENT_YEAR = date.today().year
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
 ckeditor = CKEditor(app)
 Bootstrap(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///blog.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
